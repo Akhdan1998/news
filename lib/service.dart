ddbd2f699d6b85f749cb8d94d24dbb2ab66c4ae3
Akhdan Habibie <60088854+Akhdan1998@users.noreply.github.com>
2024-04-01 16:15:05 +0700

    deleted client service

--- lib/service.dart
+++ lib/service.dart
@@ -5,15 +5,11 @@ import 'package:news/apireturn.dart';
 import 'package:news/model.dart';
 
 class NewsServices {
-  static Future<ApiReturnNews<List<News>>?> getNews(
-      {http.Client? client}) async {
+  static Future<ApiReturnNews<List<News>>?> getNews() async {
     String baseUrl =
         'https://newsapi.org/v2/everything?q=apple&from=2024-02-26&to=2024-02-26&sortBy=popularity&apiKey=1b01c8e9ec3249f992946815df9de69f';
-    if (client == null) {
-      client = http.Client();
-    }
     String url = baseUrl;
-    var response = await client.get(Uri.parse(url), headers: {
+    var response = await http.get(Uri.parse(url), headers: {
       'Content-Type': 'application/json',
     });
     if (response.statusCode != 200) {
